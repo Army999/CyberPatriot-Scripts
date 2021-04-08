852a4449ebaa798d3ea6926419a14e85b7e5c5c6
Army999 <78039071+Army999@users.noreply.github.com>
2021-04-08 13:26:37 -0400

    Update Windows 10 Script.bat

--- Windows 10 Script.bat
+++ Windows 10 Script.bat
@@ -1,15 +1,21 @@
 @echo off
 echo Copyright (c) Army999
 
+set functions=Firewall localsecpol audit delfiles remoteDesk
+
 :Firewall
 netsh advfirewall set allprofiles state on
+netsh advfirewall reset
 goto:EOF
 
-:lsp
+:localsecpol
 net accounts /minpwlen:8
 net accounts /lockoutthreshold:5
 net accounts /maxpwage:30
 net accounts /uniquepw:5
+net accounts /lockoutduration:30
+net accounts /lockoutthreshold:3
+net accounts /lockoutwindow:30
 start secpol.msc /wait
 pause 
 goto:EOF
@@ -36,3 +42,6 @@ del *.jpg
 del *.jpeg
 goto:EOF
 
+:remoteDesk
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+goto:EOF
