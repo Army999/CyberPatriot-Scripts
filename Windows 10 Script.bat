6447a6e61d858ef50a071475c4335ba1bc44c21a
Army999 <78039071+Army999@users.noreply.github.com>
2021-04-09 09:56:54 -0400

    Update Windows 10 Script.bat

--- Windows 10 Script.bat
+++ Windows 10 Script.bat
@@ -75,3 +75,57 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fAllowToGetHe
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
 netsh advfirewall firewall set rule group="remote desktop" new enable=no
 goto:EOF
+
+:insremoval
+dism /online /disable-feature /featurename:IIS-WebServerRole
+dism /online /disable-feature /featurename:IIS-WebServer
+dism /online /disable-feature /featurename:IIS-CommonHttpFeatures
+dism /online /disable-feature /featurename:IIS-HttpErrors
+dism /online /disable-feature /featurename:IIS-HttpRedirect
+dism /online /disable-feature /featurename:IIS-ApplicationDevelopment
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility45
+dism /online /disable-feature /featurename:IIS-HealthAndDiagnostics
+dism /online /disable-feature /featurename:IIS-HttpLogging
+dism /online /disable-feature /featurename:IIS-LoggingLibraries
+dism /online /disable-feature /featurename:IIS-RequestMonitor
+dism /online /disable-feature /featurename:IIS-HttpTracing
+dism /online /disable-feature /featurename:IIS-Security
+dism /online /disable-feature /featurename:IIS-URLAuthorization
+dism /online /disable-feature /featurename:IIS-RequestFiltering
+dism /online /disable-feature /featurename:IIS-IPSecurity
+dism /online /disable-feature /featurename:IIS-Performance
+dism /online /disable-feature /featurename:IIS-HttpCompressionDynamic
+dism /online /disable-feature /featurename:IIS-WebServerManagementTools
+dism /online /disable-feature /featurename:IIS-ManagementScriptingTools
+dism /online /disable-feature /featurename:IIS-IIS6ManagementCompatibility
+dism /online /disable-feature /featurename:IIS-Metabase
+dism /online /disable-feature /featurename:IIS-HostableWebCore
+dism /online /disable-feature /featurename:IIS-StaticContent
+dism /online /disable-feature /featurename:IIS-DefaultDocument
+dism /online /disable-feature /featurename:IIS-DirectoryBrowsing
+dism /online /disable-feature /featurename:IIS-WebDAV
+dism /online /disable-feature /featurename:IIS-WebSockets
+dism /online /disable-feature /featurename:IIS-ApplicationInit
+dism /online /disable-feature /featurename:IIS-ASPNET
+dism /online /disable-feature /featurename:IIS-ASPNET45
+dism /online /disable-feature /featurename:IIS-ASP
+dism /online /disable-feature /featurename:IIS-CGI
+dism /online /disable-feature /featurename:IIS-ISAPIExtensions
+dism /online /disable-feature /featurename:IIS-ISAPIFilter
+dism /online /disable-feature /featurename:IIS-ServerSideIncludes
+dism /online /disable-feature /featurename:IIS-CustomLogging
+dism /online /disable-feature /featurename:IIS-BasicAuthentication
+dism /online /disable-feature /featurename:IIS-HttpCompressionStatic
+dism /online /disable-feature /featurename:IIS-ManagementConsole
+dism /online /disable-feature /featurename:IIS-ManagementService
+dism /online /disable-feature /featurename:IIS-WMICompatibility
+dism /online /disable-feature /featurename:IIS-LegacyScripts
+dism /online /disable-feature /featurename:IIS-LegacySnapIn
+dism /online /disable-feature /featurename:IIS-FTPServer
+dism /online /disable-feature /featurename:IIS-FTPSvc
+dism /online /disable-feature /featurename:IIS-FTPExtensibility
+dism /online /disable-feature /featurename:TFTP
+dism /online /disable-feature /featurename:TelnetClient
+dism /online /disable-feature /featurename:TelnetServer
+goto:EOF
